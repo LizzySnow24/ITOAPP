c4098378d85a8e8a67768516496a3cfb0584d809
LizzySnow24 <fridaelizabethgonzalez@gmail.com>
2024-04-11 19:23:58 -0600

    entrar a la pagina del CLE

--- app/src/main/java/com/example/itoapp/Fragment_Optiones.java
+++ app/src/main/java/com/example/itoapp/Fragment_Optiones.java
@@ -1,12 +1,18 @@
 package com.example.itoapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
 public class Fragment_Optiones extends Fragment {
 
     public Fragment_Optiones() {
@@ -14,13 +20,24 @@ public class Fragment_Optiones extends Fragment {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment__optiones, container, false);
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        LinearLayout menuLayout = view.findViewById(R.id.Opcion_CLE);
+
+        menuLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://sites.google.com/view/itocle/p%C3%A1gina-principal"; // URL de CLE
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
     }
 }
